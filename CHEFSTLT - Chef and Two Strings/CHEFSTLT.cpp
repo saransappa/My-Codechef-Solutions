56221e6a232fce7258694981e49fa1551b98c0ee
saransappa <saran.sappa@gmail.com>
2021-06-05 13:28:52 +0530

    added CHEFSTLT

---
+++ CHEFSTLT - Chef and Two Strings/CHEFSTLT.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define deb(x) cout<<#x<<" "<<x<<endl; // Debugging function
+#define ll long long 
+#define pb push_back
+#define elif else if
+
+template <typename... T>
+void read(T&... args){          // Function to read multiple arguments
+    ((cin>>args), ...);
+}
+
+template <typename... T>
+void write(T&... args){          // Function to write multiple arguments
+    ((cout<<args<<" "), ...);
+}
+
+int gcd(int a, int b) 
+{ 
+    if (a == 0) 
+        return b; 
+    return gcd(b % a, a); 
+} 
+
+int lcm(int a, int b){
+    return (a*b)/gcd(a,b);
+}
+
+/*Code starts from here */
+
+void solve(){
+    string s,t;
+    cin>>s>>t;
+    int diffs = 0;
+    int unreadable = 0;
+    for(int i=0;i<s.size();i++){
+        if(s[i]=='?' || t[i]=='?'){unreadable++;continue;}
+        if(s[i]!=t[i])diffs++;
+    }
+    cout<<diffs<<" "<<(diffs+unreadable)<<endl;;
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    int t;   
+    cin>>t;
+    for(int y=0;y<t;y++){
+        solve();
+    }
+	return 0;
+}
